c033cccf3b0052aafe4d6289cb146d96d870b339
Colin Rent <colinrent@Colins-MacBook-Pro.local>
2024-02-07 00:41:40 -0500

    added script

--- script.js
+++ script.js
@@ -1,59 +1,83 @@
 const apiKey = 'b85ba95ce4b9c8578a16802cfecb6689';
-const apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${apiKey}`;
-const searchBox = document.querySelector('.search-box');
-searchBox.addEventListener('keypress', setQuery);
+const apiUrl = (`https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}`)
 
-function setQuery(evt) {
-    if (evt.keyCode == 13) {
-        getResults(searchBox.value);
+fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log('error', error));
+
+var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+console.log(searchHistory);
+
+fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log('error', error));
+
+function getWeather(cityName) {
+    var apiKey
+    var apiUrl = (`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`)
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.log('error', error));
+}
+
+function getForecast(cityName) {
+    var apiKey
+    var apiUrl = (`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`)
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.log('error', error));
+}
+
+function searchCity(event) {
+    event.preventDefault();
+    var cityName = searchCityInput.val().trim();
+    if (cityName) {
+        getWeather(cityName);
+        getForecast(cityName);
+        searchHistory.unshift({ cityName });
+        searchCityInput.val("");
     }
 }
 
-function getResults(query) {
-    fetch(`${apiUrl}?q=${query}&appid=${apiKey}`)
-        .then(response => response.json())
-        .then(displayResults);
+function clearHistory(event) {
+    event.preventDefault();
+    searchHistory = [];
+    renderHistory();
 }
 
-function displayResults(weather) {
-    let city = document.querySelector('.location .city');
-    city.innerText = `${weather.name}, ${weather.sys.country}`;
-
-    let now = new Date();
-    let date = document.querySelector('.location .date');
-    date.innerText = dateBuilder(now);
-
-    let temp = document.querySelector('.current .temp');
-    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
-
-    let weather_el = document.querySelector('.current .weather');
-    weather_el.innerText = weather.weather[0].main;
-
-    let hilow = document.querySelector('.hi-low');
-    hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
+function renderHistory() {
+    searchHistoryList.empty();
+    for (var i = 0; i < searchHistory.length; i++) {
+        var historyItem = $("<input>")
+            .attr("type", "text")
+            .attr("readonly", true)
+            .attr("class", "form-control d-block bg-white")
+            .attr("value", searchHistory[i].cityName);
+        searchHistoryList.append(historyItem);
+    }
 }
 
-function dateBuilder(d) {
-    let months = ["January", "February", "March", "April", "May", "June", "July", 
-    "August", "September", "October", "November", "December"];
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
-
-    return `${day} ${date} ${month} ${year}`;
+function init() {
+    var storedHistory = JSON.parse(localStorage.getItem("search"));
+    if (storedHistory !== null) {
+        searchHistory = storedHistory;
+    }
+    renderHistory();
 }
 
+function storeHistory() {
 
+    localStorage.setItem("search", JSON.stringify(searchHistory));
+}
 
-
-
-
-                
-
-
-
-
-
+function historyClick(event) {
+    var target = $(event.target);
+    if (target.is("input")) {
+        getWeather(target.val());
+        getForecast(target.val());
+    }
+}
