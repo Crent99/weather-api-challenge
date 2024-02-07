46ed02332e464e5ca5c00cf618dddc99053a01ab
Colin Rent <colinrent@Colins-MacBook-Pro.local>
2024-02-07 02:20:27 -0500

    added to script file

--- script.js
+++ script.js
@@ -1,83 +1,19 @@
-const apiKey = 'b85ba95ce4b9c8578a16802cfecb6689';
-const apiUrl = (`https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}`)
+const cityInput = document.querySelector('.search-btn');
+const searchButton = document.querySelector('.search-btn');
 
-fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.log('error', error));
+const API_KEY = "b85ba95ce4b9c8578a16802cfecb6689" // API key for OpenWeatherMap API
 
-var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
-console.log(searchHistory);
+const getCityCoordinates = () => {
+    const cityName = cityInput.value.trim(); // Get user entered city name and remove extra spaces
+    if (!cityName) return; // If city name is empty, return
+    const GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
 
-fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.log('error', error));
-
-function getWeather(cityName) {
-    var apiKey
-    var apiUrl = (`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`)
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.log('error', error));
+    fetch(GEOCODING_API_URL).then(res => res.json()).then(data => {
+        console.log(data)
+    }).catch(() => {
+        alert("An error occured while fetching the coordinates!");
+    });
 }
+    
 
-function getForecast(cityName) {
-    var apiKey
-    var apiUrl = (`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`)
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.log('error', error));
-}
-
-function searchCity(event) {
-    event.preventDefault();
-    var cityName = searchCityInput.val().trim();
-    if (cityName) {
-        getWeather(cityName);
-        getForecast(cityName);
-        searchHistory.unshift({ cityName });
-        searchCityInput.val("");
-    }
-}
-
-function clearHistory(event) {
-    event.preventDefault();
-    searchHistory = [];
-    renderHistory();
-}
-
-function renderHistory() {
-    searchHistoryList.empty();
-    for (var i = 0; i < searchHistory.length; i++) {
-        var historyItem = $("<input>")
-            .attr("type", "text")
-            .attr("readonly", true)
-            .attr("class", "form-control d-block bg-white")
-            .attr("value", searchHistory[i].cityName);
-        searchHistoryList.append(historyItem);
-    }
-}
-
-function init() {
-    var storedHistory = JSON.parse(localStorage.getItem("search"));
-    if (storedHistory !== null) {
-        searchHistory = storedHistory;
-    }
-    renderHistory();
-}
-
-function storeHistory() {
-
-    localStorage.setItem("search", JSON.stringify(searchHistory));
-}
-
-function historyClick(event) {
-    var target = $(event.target);
-    if (target.is("input")) {
-        getWeather(target.val());
-        getForecast(target.val());
-    }
-}
+searchButton.addEventListener('click', getCityCoordinates);
